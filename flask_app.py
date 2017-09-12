fcb2fd883de9e51cd0b6560fbffaac80de925628
Mohammad <mohoshir@gmail.com>
2017-09-12 18:54:18 +0000

    added output html file for output

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# request function: provides a global context which is used to
-# access information about the latest request made to our application
-# tion. GET arguments that user passes along as part of a
-# request are automatically available in request.args, from which we can access
-# key-value pairs as we would with a Python dictionary
-
 # render_template: takes a Jinja template as input and produces pure HTML
-
-from itertools import permutations
 from flask import render_template, request
 
 # added my own function, this function will remove duplicate words, if we have duplicate characters in input
@@ -28,18 +20,24 @@ def my_perm(y):
 # want my default page to be accessible by either GET or POST
 @app.route('/', methods=['GET', 'POST'])
 def output():
-    result="hi"
+    input="hi"
     # Change request.args.get to request.form.get
     if request.form.get('perm', None):
-        result = request.form['perm']
-        # perms = [''.join(p) for p in permutations(result)]
-        perms = [''.join(p) for p in my_perm(result)]
-        return str(perms)
+        input = request.form['perm']
+        result = my_perm(input)
+        perms = [''.join(p) for p in result]
+        dedups = len(perms)
+        output = {'output_cell': str(perms)}
+        return render_template('output.html',
+                                title='Output',
+                                input_word=input,
+                                ana_dups=dedups,
+                                output_html=output)
     else:
         user = {'nickname': 'Dear User'}
         return render_template('index.html',
                                 title='Home',
-                                user=user)
+                                user_html=user)
 
 
 
